31032f55938a89d0c3017890adb26d2efb879733
lucasmelogithub <lucas.melo@intel.com>
2023-01-04 22:06:47 -0600

    Add test example code

--- examples/linux-service-plan/main.tf
+++ examples/linux-service-plan/main.tf
@@ -1,7 +1,44 @@
-module "linux-service-plan" {
+module "linux-service-plan-east-v3" {
   source              = "../../"
   service_plan_name   = "intel-linux-service-plan-110"
   resource_group_name = "terraform-testing-rg"
+  tags = {
+    "Owner"    = "Intel.Cloud.Modules@intel.com"
+    "Duration" = "4"
+  }
+}
+
+module "linux-service-plan-west-v3" {
+  source              = "../../"
+  service_plan_name   = "intel-linux-service-plan-110"
+  resource_group_name = "terraform-testing-west-rg"
+  zone_balancing_enabled = false
+  tags = {
+    "Owner"    = "Intel.Cloud.Modules@intel.com"
+    "Duration" = "4"
+  }
+}
+
+
+#V2
+
+module "linux-service-plan-east-v2" {
+  source              = "../../"
+  service_plan_name   = "intel-linux-service-plan-110-v2"
+  resource_group_name = "terraform-testing-rg"
+  sku_name = "P2v2"
+  tags = {
+    "Owner"    = "Intel.Cloud.Modules@intel.com"
+    "Duration" = "4"
+  }
+}
+
+module "linux-service-plan-west-v2" {
+  source              = "../../"
+  service_plan_name   = "intel-linux-service-plan-110-v2"
+  resource_group_name = "terraform-testing-west-rg"
+  sku_name = "P2v2"
+  zone_balancing_enabled = false
   tags = {
     "Owner"    = "Intel.Cloud.Modules@intel.com"
     "Duration" = "4"
